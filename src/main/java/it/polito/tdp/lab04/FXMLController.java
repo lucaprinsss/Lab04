d39948ad407f9174e69fe209063e21be5405698c
luca <luca@172.22.81.138>
2022-03-30 10:04:10 +0200

    Turno del 29/03

--- src/main/java/it/polito/tdp/lab04/FXMLController.java
+++ src/main/java/it/polito/tdp/lab04/FXMLController.java
@@ -6,9 +6,20 @@ package it.polito.tdp.lab04;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.lab04.DAO.CorsoDAO;
+import it.polito.tdp.lab04.model.Corso;
+import it.polito.tdp.lab04.model.Model;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
+	
+	private Model model;
 
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
@@ -16,8 +27,83 @@ public class FXMLController {
     @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
+    @FXML // fx:id="btbCercaIscrittiCorso"
+    private Button btbCercaIscrittiCorso; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnCercaCorsi"
+    private Button btnCercaCorsi; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnIscrivi"
+    private Button btnIscrivi; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnMatricola"
+    private Button btnMatricola; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnReset"
+    private Button btnReset; // Value injected by FXMLLoader
+
+    @FXML // fx:id="cmbCorso"
+    private ComboBox<String> cmbCorso; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtArea"
+    private TextArea txtArea; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtCognome"
+    private TextField txtCognome; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtMatricola"
+    private TextField txtMatricola; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtNome"
+    private TextField txtNome; // Value injected by FXMLLoader
+
+    @FXML
+    void cercaNomeCognome(ActionEvent event) {
 
     }
+
+    @FXML
+    void doCercaCorsi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCercaIscittiCorso(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doIscrivi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doreset(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert btbCercaIscrittiCorso != null : "fx:id=\"btbCercaIscrittiCorso\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCercaCorsi != null : "fx:id=\"btnCercaCorsi\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnIscrivi != null : "fx:id=\"btnIscrivi\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnMatricola != null : "fx:id=\"btnMatricola\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert cmbCorso != null : "fx:id=\"cmbCorso\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtArea != null : "fx:id=\"txtArea\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtCognome != null : "fx:id=\"txtCognome\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtMatricola != null : "fx:id=\"txtMatricola\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtNome != null : "fx:id=\"txtNome\" was not injected: check your FXML file 'Scene.fxml'.";
+
+    }
+    
+    public void setModel (Model model) {
+    	this.model=model;
+    	cmbCorso.getItems().clear();
+    	cmbCorso.getItems().add("");
+    	for(Corso c: model.getTuttiICorsi()) {
+    		cmbCorso.getItems().add(c.getCodins());
+    	}
+    }
+
 }
--- src/main/java/it/polito/tdp/lab04/model/Model.java
+++ src/main/java/it/polito/tdp/lab04/model/Model.java
@@ -1,5 +1,17 @@
 package it.polito.tdp.lab04.model;
 
+import java.util.List;
+
+import it.polito.tdp.lab04.DAO.CorsoDAO;
+
 public class Model {
+	
+	CorsoDAO corsoDAO=new CorsoDAO();
+	
+	public List<Corso> getTuttiICorsi() {
+		return corsoDAO.getTuttiICorsi();
+	}
+	
+	
 
 }
